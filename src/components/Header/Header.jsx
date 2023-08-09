6679dc755ec82a2be00fecb3417ff8734d542509
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-08-09 17:33:01 +0530

    Creating Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
+import "./Header.css";
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header-container"> 
+      <div className="navbar">
+        <div className="navbar-logo">
+          <img src="./peppermintLogo.png" />
+        </div>
+        <div className='navbar-header'>
+          <h1>Peppermint Robotics</h1>
+        </div>
+      </div>
+      <div className='nav-links'>
+        <Link to="/">Home</Link>
+        <Link to="/careers">Careers</Link>
+      </div>
+    </div>
   )
 }
 
